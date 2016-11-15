93ef259d3478d804aa667a903828799e3672f06c
agdhruv <dhruv_agarwal@live.com>
2016-11-16 00:06:37 +0530

    Adds proper CSS for index page.

--- index.php
+++ index.php
@@ -12,40 +12,59 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Thor - Ashoka's Online Judge</title>
-	<link rel="stylesheet" href="css/common.css">
+	<link rel="stylesheet" href="css/index.css">
 </head>
 <body>
-	<?php 
-		if(isset($_SESSION["user"])){
-			echo "Logged in as ".$_SESSION["user"];
-			echo '<a href="submit-code.php">Submit Code</a>';
-			echo '<a href="logout.php">Logout</a>';
-		}
-		else{
-			echo '<a href="login.php">Login</a>';
-			echo '<a href="register.php">Register</a>';
-		}
-	?>
-	<table>
-		<tr>
-			<th>Rank</th>
-			<th>Username</th>
-			<th>Score</th>
-		</tr>
-		<?php
-			$query = "SELECT * FROM users ORDER BY score DESC";
-    		$result = mysqli_query($conn,$query);
-    		$rank = 0;
-			while($data = mysqli_fetch_assoc($result)){
-				echo "<tr>";
-				echo "<td>".$rank."</td>";
-				echo "<td>".$data["UID"]."</td>";
-				echo "<td>".$data["score"]."</td>";
-				echo "</tr>";
-				$rank++;
-			}
-		?>
-	</table>
+	<nav>
+		<h1 class="title">
+			Thor
+		</h1>
+		<div class="links">
+			<?php 
+				if(isset($_SESSION["user"])){
+					echo '<a href="javascript:void(0);">'.$_SESSION["user"].'</a>';
+					echo '<a href="submit-code.php"><span>Submit Code</span></a>';
+					echo '<a href="logout.php"><span>Logout</span></a>';
+				}
+				else{
+					echo '<a href="login.php"><span>Login</span></a>';
+					echo '<a href="register.php"><span>Register</span></a>';
+				}
+			?>
+		</div>
+	</nav>
+	<section class="section1">
+		<h1>Current Rank List</h1>
+		<table class="rankTable">
+			<tr>
+				<th>Rank</th>
+				<th>Name</th>
+				<th>Username</th>
+				<th>Score</th>
+				<th>Accuracy</th>
+			</tr>
+			<?php
+				$query = "SELECT * FROM users ORDER BY score DESC";
+	    		$result = mysqli_query($conn,$query);
+	    		$rank = 0;
+				while($data = mysqli_fetch_assoc($result)){
+					if(isset($_SESSION["user"])&&($data["UID"]==$_SESSION["user"])){
+						echo '<tr class="loggedIn">';
+					}
+					else{
+						echo '<tr class="notLoggedIn">';
+					}
+					echo "	<td>".$rank."</td>";
+					echo "	<td>".$data["name"]."</td>";
+					echo "	<td>".$data["UID"]."</td>";
+					echo "	<td>".$data["score"]."</td>";
+					echo "	<td>".$data["accuracy"]."</td>";
+					echo "</tr>";
+					$rank++;
+				}
+			?>
+		</table>
+	</section>
 </body>
 <?php
 	mysqli_close($conn);
