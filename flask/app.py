4b7e7670fc80c2661fe02d60e60c4125a13db292
Barun Parruck <barun.parruck@gmail.com>
2016-11-18 18:25:54 +0530

    Adds C++ language functionality
    The checker can now successfully judge Cpp code. API must now have language_id passed to it in the form of a string ("cpp"/"c"/"java"/"py)
    Note that only cpp and py are currently supported

--- flask/app.py
+++ flask/app.py
@@ -4,14 +4,29 @@ import os
 import filecmp
 app = Flask(__name__)
 
-@app.route('/judge/<submission_id>/<input_id>/<exoutput_id>/<int:timeout>', methods=['GET']) # time limit = timeout
-def index(submission_id, input_id, exoutput_id,timeout):
-    submission_path = "submissions/" + str(submission_id) + ".py"
+@app.route('/judge/<submission_id>/<input_id>/<exoutput_id>/<int:timeout>/<language_id>', methods=['GET']) # time limit = timeout
+def index(submission_id, input_id, exoutput_id,timeout,language_id):
+    submission_path = "submissions/" + str(submission_id) + "." + language_id
     input_path = "inputs/" + str(input_id) + ".txt"
     print(input_path)
     exoutput_path = "exoutputs/" + str(exoutput_id) + ".txt"
     output_path = "outputs/" + str(submission_id) + ".txt"
-    r = os.system("timeout " + str(timeout) + " python3 " + submission_path + " < " + input_path + " > " + output_path)
+    r = -1
+    if language_id=="py":
+        r = os.system("timeout " + str(timeout) + " python3 " + submission_path + " < " + input_path + " > " + output_path)
+    elif language_id=="cpp":
+        r = os.system("timeout " + str(timeout) + " g++ -o " + "binaries/" + submission_id + " " + submission_path)
+        if r!=0:
+            return "CE"
+        r = os.system("timeout " + str(timeout) + " ./" + "binaries/" + submission_id + " < " + input_path + " > " + output_path)
+        if r==31744:
+            return "TLE"
+        elif r!=0:
+            return "RTE"
+        elif filecmp.cmp(output_path,exoutput_path):
+            return "AC"
+        else:
+            return "WA"
     if r==31744: # the error code returned when timeout times out. Haha
         return "TLE"
     elif r!=0:
