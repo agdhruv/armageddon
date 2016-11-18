bb318ad361593c101ac490c69853e47c2d1c520d
agdhruv <dhruv_agarwal@live.com>
2016-11-19 02:16:08 +0530

    Comments out unncessary log to console.

--- submit-code.php
+++ submit-code.php
@@ -90,7 +90,7 @@
             $("#code").val(editor.getSession().getValue());//enter code editor code to text area to POST to API
             var data = $(this).serialize();
             data += "&submit=yes";
-            console.log(data);
+            //console.log(data);
             $.post("result.php",data,function(response){
                 if(response){
                     $("p.result").html("Verdict: "+response);
