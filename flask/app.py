794b4ce20270463abf5bee159a68f532d92c7980
agdhruv <dhruv_agarwal@live.com>
2017-01-08 17:00:58 +0530

    Made a small mistake. Repairs it.

--- flask/app.py
+++ flask/app.py
@@ -1,11 +1,11 @@
-#!/Library/Frameworks/Python.framework/Versions/3.5/bin/python3
+#!bin/python
 from flask import Flask
 import os
 import filecmp
 app = Flask(__name__)
 
-@app.route('/judge/<submission_id>/<input_id>/<exoutput_id>/<int:gtimeout>/<language_id>', methods=['GET']) # time limit = gtimeout
-def index(submission_id, input_id, exoutput_id,gtimeout,language_id):
+@app.route('/judge/<submission_id>/<input_id>/<exoutput_id>/<int:timeout>/<language_id>', methods=['GET']) # time limit = timeout
+def index(submission_id, input_id, exoutput_id,timeout,language_id):
     submission_path = "submissions/" + str(submission_id) + "." + language_id
     input_path = "inputs/" + str(input_id) + ".txt"
     print(input_path)
@@ -13,8 +13,8 @@ def index(submission_id, input_id, exoutput_id,gtimeout,language_id):
     output_path = "outputs/" + str(submission_id) + ".txt"
     r = -1
     if language_id=="py":
-        r = os.system("gtimeout " + str(gtimeout) + " python3 " + submission_path + " < " + input_path + " > " + output_path) 
-        if r==31744: # the error code returned when gtimeout times out. Haha
+        r = os.system("timeout " + str(timeout) + " python3 " + submission_path + " < " + input_path + " > " + output_path) 
+        if r==31744: # the error code returned when timeout times out. Haha
             return "TLE"
         elif r!=0:
             return "CE"
@@ -23,10 +23,10 @@ def index(submission_id, input_id, exoutput_id,gtimeout,language_id):
         else:
             return "WA"              
     elif language_id=="cpp":
-        r = os.system("gtimeout " + str(gtimeout) + " g++ -o " + "binaries/" + submission_id + " " + submission_path + " -std=c++14 -DONLINE_JUDGE")
+        r = os.system("timeout " + str(timeout) + " g++ -o " + "binaries/" + submission_id + " " + submission_path + " -std=c++14 -DONLINE_JUDGE")
         if r!=0:
             return "CE"
-        r = os.system("gtimeout " + str(gtimeout) + " ./" + "binaries/" + submission_id + " < " + input_path + " > " + output_path)
+        r = os.system("timeout " + str(timeout) + " ./" + "binaries/" + submission_id + " < " + input_path + " > " + output_path)
         if r==31744:
             return "TLE"
         elif r!=0:
@@ -36,10 +36,10 @@ def index(submission_id, input_id, exoutput_id,gtimeout,language_id):
         else:
             return "WA"
     elif language_id=="c":
-        r = os.system("gtimeout " + str(gtimeout) + " gcc -o " + "binaries/" + submission_id + " " + submission_path)
+        r = os.system("timeout " + str(timeout) + " gcc -o " + "binaries/" + submission_id + " " + submission_path)
         if r!=0:
             return "CE"
-        r = os.system("gtimeout " + str(gtimeout) + " ./" + "binaries/" + submission_id + " < " + input_path + " > " + output_path)
+        r = os.system("timeout " + str(timeout) + " ./" + "binaries/" + submission_id + " < " + input_path + " > " + output_path)
         if r==31744:
             return "TLE"
         elif r!=0:
@@ -50,12 +50,12 @@ def index(submission_id, input_id, exoutput_id,gtimeout,language_id):
             return "WA"
     elif language_id=="java":
         os.system("mv " + submission_path + " submissions/Main.java")
-        r = os.system("gtimeout " + str(gtimeout) + " javac submissions/Main.java")
+        r = os.system("timeout " + str(timeout) + " javac submissions/Main.java")
         k = "ERROR JAVA-511: Please report this issue"
         if r!=0:
             k = "CE"
         else:
-            r = os.system("gtimeout " + str(gtimeout) + " java -cp submissions/ Main < " + input_path + " > " + output_path)
+            r = os.system("timeout " + str(timeout) + " java -cp submissions/ Main < " + input_path + " > " + output_path)
             if r==31744:
                 k = "TLE"
             elif r!=0:
